6832e999570c81c16ee8010640531b82c2893538
Sanket Landage <sanket.landage@gmail.com>
2021-07-12 09:16:10 +0530

    Initial Commit

---
+++ src/main/java/com/cg/census/profile/CensusProfiling/CensusProfilingApplication.java
@@ -0,0 +1,13 @@
+package com.cg.census.profile.CensusProfiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensusProfilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensusProfilingApplication.class, args);
+	}
+
+}
