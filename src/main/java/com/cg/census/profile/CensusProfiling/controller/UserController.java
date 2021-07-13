bd4c6d31b8dfc7afffc347ace8a5aa3827788851
Sanket Landage <sanket.landage@gmail.com>
2021-07-13 18:00:04 +0530

    Update UserController.java

--- src/main/java/com/cg/census/profile/CensusProfiling/controller/UserController.java
+++ src/main/java/com/cg/census/profile/CensusProfiling/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,24 @@ public class UserController  {
 	public List<User> getAllUsers() {
 		LOG.info("AllUsers");
 		return service.findAllUsers();
+	}
+	
+	
+	@GetMapping("/getbyfirstname/{firstName}")
+	public List<User> getByFirstName(@PathVariable("firstName") String firstName) {
+		LOG.info("getByFirstName");
+		return service.findUserByFirstName(firstName);
+	}
+
+	@GetMapping("/getbylastname/{lastName}")
+	public List<User> getByLastName(@PathVariable("lastName") String lastName) {
+		LOG.info("getBylastName");
+		return service.findUserByFirstName(lastName);
+	}
+
+	@GetMapping("/getbyemail/{email}")
+	public List<User> getByEmail(@PathVariable("email") String email) {
+		LOG.info("getByEmail");
+		return service.findUserByGender(email);
 	}
 }
