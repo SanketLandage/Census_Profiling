97c97baaff104c543dacf241d4159428f3d21973
Sanket Landage <sanket.landage@gmail.com>
2021-07-13 14:08:33 +0530

    Tables update

--- src/main/java/com/cg/census/profile/CensusProfiling/model/UserFamilyMember.java
+++ src/main/java/com/cg/census/profile/CensusProfiling/model/UserFamilyMember.java
@@ -14,101 +14,96 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 public class UserFamilyMember {
 	@Id
 	private int memId;
-	
+
 	@Column
 	private String memFirstName;
-	
+
 	@Column
 	private String memLastName;
-	
+
 	@Column
 	private String memGender;
-	
+
+	@Column
+	private String relation;
+
 	@Column
 	private String dob;
-	
+
 	@JsonBackReference
 	@ManyToOne
 	private User user;
-	
-	
 
-
-	
 	public UserFamilyMember() {
 		super();
 	}
 
-
-	public UserFamilyMember( int memId,String memFirstName, String memLastName, String memGender, String dob,User user
-			) {
+	public UserFamilyMember(int memId, String memFirstName, String memLastName, String memGender, String relation,
+			String dob, User user) {
 		super();
 		this.memId = memId;
 		this.memFirstName = memFirstName;
 		this.memLastName = memLastName;
 		this.memGender = memGender;
+		this.relation = relation;
 		this.dob = dob;
 		this.user = user;
 	}
-
 
 	public int getMemId() {
 		return memId;
 	}
 
-
 	public void setMemId(int memId) {
 		this.memId = memId;
 	}
-
 
 	public String getMemFirstName() {
 		return memFirstName;
 	}
 
-
 	public void setMemFirstName(String memFirstName) {
 		this.memFirstName = memFirstName;
 	}
-
 
 	public String getMemLastName() {
 		return memLastName;
 	}
 
-
 	public void setMemLastName(String memLastName) {
 		this.memLastName = memLastName;
 	}
-
 
 	public String getMemGender() {
 		return memGender;
 	}
 
-
 	public void setMemGender(String memGender) {
 		this.memGender = memGender;
 	}
-
 
 	public String getDob() {
 		return dob;
 	}
 
-
 	public void setDob(String dob) {
 		this.dob = dob;
 	}
-
 
 	public User getUser() {
 		return user;
 	}
 
-
 	public void setUser(User user) {
 		this.user = user;
 	}
-		
+
+	public String getRelation() {
+		return relation;
+	}
+
+	public void setRelation(String relation) {
+		this.relation = relation;
+	}
+
 }
